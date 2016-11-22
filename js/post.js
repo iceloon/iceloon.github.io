605ca93d8d5b67ca0c5afda6c4b331912eb49c27
iceloon <iceloon@gmail.com>
2016-11-22 12:36:43 +0800

    Update post.js

--- js/post.js
+++ js/post.js
@@ -25,11 +25,13 @@ $(document).ready(function(){
 
     //***********************
     
-    window.disqus_shortname = 'sun2lang'; // required: replace example with your forum shortname
+    //var duoshuoQuery = {short_name:"iceloon"}; // required: replace example with your forum shortname
+    var sun = $('#disqus_container .ds-thread');
     $('#disqus_container .comment').on('click',function(){
         $(this).html('加载中...');
         var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+        this.remove();
+        sun.show()
     });
  
     //***********************
